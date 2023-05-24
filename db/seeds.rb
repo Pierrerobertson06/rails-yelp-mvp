2805fe724e5893ee64fd32e90dd61aa786e5e003
pierre robertson <pierrerobertson06@gmail.com>
2023-05-24 17:00:30 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# coco = Restaurant.create!(name: "coco", address: "paris, 75010", category: "french")
+# beaurivage = Restaurant.create!(name: "beaurivage", address: "lausanne, 1000", category: "italian")
+# annesophie = Restaurant.create!(name: "annesophie", address: "ours, 1060", category: "chinese")
+boco = Restaurant.create!(name: "boco", address: "bruxelle, 3000", category: "belgian")
+pepe = Restaurant.create!(name: "pepe", address: "tokyo, 10383", category: "japanese")
